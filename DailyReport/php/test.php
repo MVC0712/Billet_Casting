ecf4da1f35619c282f7c5d182737740c689f2f18
Donnie <mongvanchien@gmail.com>
2022-05-24 17:51:17 +0700

    add connection

---
+++ DailyReport/php/test.php
@@ -0,0 +1,26 @@
+<?php
+require_once('../../connection.php');
+
+$dbh = new DBHandler();
+
+if ($dbh->getInstance() === null) {
+    die("No database connection");
+}
+
+$datetime = date("Y-m-d H:i:s");
+
+try {
+    $sql = "SELECT * FROM m_dies;
+    )";
+
+    $stmt = $dbh->getInstance()->prepare($sql);
+
+    $stmt->execute();
+
+    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+
+} 
+catch(PDOException $e) {
+    echo $e;
+}
+?>
