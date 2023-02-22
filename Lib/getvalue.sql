47b7c4f481d1271fc59cdde2c8d2752592dfe8a8
Chien Mong Van <chien.mv@smcmfg.com.vn>
2023-02-22 16:06:24 +0700

    Get value sql

--- Lib/getvalue.sql
+++ Lib/getvalue.sql
@@ -10,6 +10,16 @@ SELECT
     t100.tt_ppd,
     t1000.tt_ppdd,
     t10000.tt_ppig,
+    SUM(input_cr_1 + input_cr_2 + input_cu_1 + input_cu_2 + input_fe_1 + input_fe_2 + input_mg_1 + input_mg_2 + input_mn_1 + input_mn_2 + input_si_1 + input_si_2 + input_ti_b_1 + input_ti_b_2 + input_zn_1 + input_zn_2 + IFNULL(t100000.tt_pphk, 0)) AS total_input_hk,
+    SUM(input_cr_1 + input_cr_2) AS input_cr,
+    SUM(input_cu_1 + input_cu_2) AS input_cu,
+    SUM(input_fe_1 + input_fe_2) AS input_fe,
+    SUM(input_mg_1 + input_mg_2) AS input_mg,
+    SUM(input_mn_1 + input_mn_2) AS input_mn,
+    SUM(input_si_1 + input_si_2) AS input_si,
+    SUM(input_ti_b_1 + input_ti_b_1) AS input_ti,
+    SUM(input_zn_1 + input_zn_2) AS input_zn,
+    t100000.tt_pphk,
     t_casting.melting_gas_end - t_casting.melting_gas_start AS gas_used
 FROM
     t_casting
@@ -125,11 +135,26 @@ FROM
     m_material_name ON m_material_name_type.material_name_id = m_material_name.id
 WHERE
     m_material_name.id = 6
-        OR m_material_name.id = 7
 GROUP BY t_add_material.t_casting) t10000 ON t_casting.id = t10000.ppig_id
+LEFT JOIN
+    (SELECT 
+    t_add_material.t_casting AS ppig_id,
+    t_casting.code,
+    SUM(weight) AS tt_pphk
+FROM
+    billet_casting.t_add_material
         LEFT JOIN
+    t_casting ON t_casting.id = t_add_material.t_casting
+        LEFT JOIN
+    m_material_name_type ON m_material_name_type.id = t_add_material.material_type
+        LEFT JOIN
+    m_material_name ON m_material_name_type.material_name_id = m_material_name.id
+WHERE
+	m_material_name.id = 7
+GROUP BY t_add_material.t_casting) t100000 ON t_casting.id = t100000.ppig_id
+LEFT JOIN
     m_material_type ON m_material_type.id = t_casting.product_type
-        LEFT JOIN
+LEFT JOIN
     t_plan ON t_plan.code = t_casting.code
 GROUP BY t_casting.id
 ORDER BY t_casting.product_date DESC;
